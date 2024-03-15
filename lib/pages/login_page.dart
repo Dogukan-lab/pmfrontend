59c788f6751601ceaed4d876b13717af12a1b3d7
LarsingDash <l.villevoye@student.avans.nl>
2024-03-15 11:42:54 +0100

    Background

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,21 +6,67 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Stack(
       children: [
-        Expanded(
-          flex: 5,
-          child: Container(
-            color: Colors.grey,
-            child: Column(
-              children: [
-                const Text('Pale Messenger'),
-                LoginInputs(),
-              ],
+        Stack(
+          children: [
+            Image.asset(
+              'backgrounds/login_background.png',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
             ),
-          ),
+            Transform(
+              transform: Matrix4.identity()..scale(2.0, 1.0),
+              alignment: Alignment.center,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                    colors: [
+                      const Color.fromARGB(90, 226, 149, 93),
+                      Colors.black.withOpacity(0.5),
+                    ],
+                    stops: const [0.5, 1.0],
+                    center: Alignment.center,
+                    radius: 0.5,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        const Expanded(flex: 8, child: SizedBox.shrink())
+        Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(181, 24, 24, 24),
+                      Color.fromARGB(181, 48, 48, 48),
+                      Color.fromARGB(181, 67, 67, 67),
+                      Color.fromARGB(143, 74, 74, 74),
+                      Color.fromARGB(18, 95, 95, 95),
+                    ],
+                    // stops: [0.0, 0.6233, 0.7027, 0.8752, 1.0], //better?
+                    stops: [0.0, 0.6233, 0.9027, 0.9752, 1.0],
+                  ),
+                ),
+                child: const Column(
+                  children: [
+                    Text(
+                      'Pale Messenger',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    LoginInputs(),
+                  ],
+                ),
+              ),
+            ),
+            const Expanded(flex: 5, child: SizedBox.shrink())
+          ],
+        ),
       ],
     );
   }
