6c0a19710ce8f4dac7efff8f9cb7b65bac7bc0a6
LarsingDash <l.villevoye@student.avans.nl>
2024-03-17 16:52:26 +0100

    Registration

--- lib/domain/usecases/login_and_register_usecase.dart
+++ lib/domain/usecases/login_and_register_usecase.dart
@@ -4,8 +4,10 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pmfrontend/data/DTOs/pm_user.dart';
 import 'package:pmfrontend/data/repositories/server_handler.dart';
+import 'package:pmfrontend/domain/entities/profile.dart';
 import 'package:pmfrontend/presentation/states/login_state.dart';
 import 'package:pmfrontend/presentation/states/page_state.dart';
+import 'package:pmfrontend/presentation/states/profile_state.dart';
 import 'package:pmfrontend/presentation/states/register_state.dart';
 
 void requestLogin(WidgetRef ref) async {
@@ -15,7 +17,7 @@ void requestLogin(WidgetRef ref) async {
   loginNotifier.changeEnum(LoginStateEnum.waiting);
 
   final response = await serverRequest(
-    'PmUsers',
+    'PmAuth/Login',
     jsonEncode(PmUser(
       username: loginState.username,
       password: loginState.password,
@@ -26,14 +28,39 @@ void requestLogin(WidgetRef ref) async {
     ref.read(pageProvider.notifier).setPage(ref, Pages.home);
   } else {
     loginNotifier.changeEnum(LoginStateEnum.incorrect);
+    await Future.delayed(const Duration(seconds: 2));
+    loginNotifier.changeEnum(LoginStateEnum.none);
   }
 }
 
 void requestRegistration(WidgetRef ref) async {
-  // final registerNotifier = ref.read(registerStateProvider.notifier);
+  final registerNotifier = ref.read(registerProvider.notifier);
   final registerState = ref.read(registerProvider);
 
-  print(registerState.username);
-  print(registerState.password);
-  print('----');
+  final response = await serverRequest(
+    'PmAuth/Register',
+    jsonEncode(PmUser(
+      username: registerState.username,
+      password: registerState.password,
+    ).toJson()),
+  );
+
+  ref.read(profileProvider.notifier).loadProfile(ProfileState(
+        profile: Profile(
+          registerState.username,
+          'Hi I\'m new!',
+          0,
+          true,
+        ),
+        background: 0,
+        friends: [],
+      ));
+
+  if (response != null && response.statusCode == HttpStatus.ok) {
+    ref.read(pageProvider.notifier).setPage(ref, Pages.home);
+  } else {
+    registerNotifier.changeEnum(RegisterStateEnum.incorrect);
+    await Future.delayed(const Duration(seconds: 2));
+    registerNotifier.changeEnum(RegisterStateEnum.none);
+  }
 }
