4beb514595fb9f02d7442215eda1fcd45f2dc80c
LarsingDash <l.villevoye@student.avans.nl>
2024-03-17 17:54:49 +0100

    Homescreen searching

--- lib/presentation/molecules/home/home_list_item.dart
+++ lib/presentation/molecules/home/home_list_item.dart
@@ -6,43 +6,47 @@ import 'package:pmfrontend/presentation/molecules/home/profile_card.dart';
 import 'package:pmfrontend/presentation/pale_themes.dart';
 
 class HomeListItem extends StatelessWidget {
-  const HomeListItem({super.key, required this.user});
+  const HomeListItem({super.key, required this.user, this.isLast = false});
 
   final Profile user;
+  final bool isLast;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: Pad.smallPlus),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ProfileCard(
-                name: user.username,
-                text: user.status,
-                icon: user.icon,
-                online: user.online,
-              ),
-              const Row(
-                children: [
-                  CustomIconButton(
-                    FontAwesomeIcons.message,
-                    iconSize: Pad.mediumPlus,
-                  ),
-                  SizedBox(width: Pad.medium),
-                  CustomIconButton(
-                    FontAwesomeIcons.ellipsisVertical,
-                    iconSize: Pad.mediumPlus,
-                  ),
-                ],
-              )
-            ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: Pad.small),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: Pad.small),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ProfileCard(
+                  name: user.username,
+                  text: user.status,
+                  icon: user.icon,
+                  online: user.online,
+                ),
+                const Row(
+                  children: [
+                    CustomIconButton(
+                      FontAwesomeIcons.message,
+                      iconSize: Pad.mediumPlus,
+                    ),
+                    SizedBox(width: Pad.medium),
+                    CustomIconButton(
+                      FontAwesomeIcons.ellipsisVertical,
+                      iconSize: Pad.mediumPlus,
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
-        const Divider(color: Cols.grey107),
-      ],
+          if (!isLast) const Divider(color: Cols.grey107),
+        ],
+      ),
     );
   }
 }
--- lib/presentation/organisms/main/home_screen.dart
+++ lib/presentation/organisms/main/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pmfrontend/domain/entities/profile.dart';
 import 'package:pmfrontend/presentation/atoms/hallownest_header.dart';
@@ -8,8 +8,16 @@ import 'package:pmfrontend/presentation/pale_themes.dart';
 import 'package:pmfrontend/presentation/states/online_users_state.dart';
 import 'package:pmfrontend/presentation/states/profile_state.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _searchController = TextEditingController();
+  String _searchTarget = '';
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +28,28 @@ class HomeScreen extends StatelessWidget {
           //Header
           const HomePageHeader('Hallownest', Cols.grey38),
 
+          //Seachbar
+          Padding(
+            padding: const EdgeInsets.fromLTRB(Pad.small, Pad.smallPlus, Pad.small, 0),
+            child: Container(
+              height: Sizes.small,
+              decoration: BoxDecoration(
+                color: Cols.grey48,
+                borderRadius: BorderRadius.circular(Radii.small),
+              ),
+              child: TextField(
+                controller: _searchController,
+                onChanged: (value) => setState(() => _searchTarget = value),
+                style: Styles.ggGrey,
+                cursorColor: Cols.grey107,
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: Pad.small),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+
           //List
           Expanded(
             child: SingleChildScrollView(
@@ -29,6 +59,7 @@ class HomeScreen extends StatelessWidget {
                   final friends = List<Profile>.from(ref.watch(profileProvider).friends);
                   final online = List<Profile>.from(ref.watch(onlineUsersProvider));
 
+                  //Remove duplicates
                   online.removeWhere((user) {
                     for (var other in friends) {
                       if (user.username == other.username) return true;
@@ -37,20 +68,33 @@ class HomeScreen extends StatelessWidget {
                     return false;
                   });
 
+                  //Searching
+                  friends.retainWhere((user) => user.username.contains(_searchTarget));
+                  online.retainWhere((user) => user.username.contains(_searchTarget));
+
+                  //List
                   return Padding(
                     padding: const EdgeInsets.fromLTRB(Pad.smallPlus, Pad.smallPlus, Pad.smallPlus, 0),
                     child: Column(
                       children: [
                         //Friends
                         const HallownestHeader('Friendly shades'),
-                        for (var user in friends) HomeListItem(user: user),
+                        for (int i = 0; i < friends.length; i++)
+                          HomeListItem(
+                            user: friends[i],
+                            isLast: i == friends.length - 1,
+                          ),
 
                         //Divider
                         const SizedBox(height: Pad.smallPlus),
 
                         //Other
                         const HallownestHeader('Other shades'),
-                        for (var user in online) HomeListItem(user: user),
+                        for (int i = 0; i < online.length; i++)
+                          HomeListItem(
+                            user: online[i],
+                            isLast: i == online.length - 1,
+                          ),
                       ],
                     ),
                   );
